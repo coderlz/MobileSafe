82e1127fad2a69f2989a7194882f0a8486804007
coderlz <chenlz527@163.com>
2016-06-08 17:46:37 +0800

    First day complete

---
+++ MyApplication/app/src/main/java/com/example/administrator/utils/StreamUtil.java
@@ -0,0 +1,32 @@
+package com.example.administrator.utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+
+/**
+ * Created by Administrator on 5/5/2016.
+ */
+public class StreamUtil {
+
+    public  static String parserStreamUtil(InputStream stream) throws IOException{
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+
+        StringWriter sw = new StringWriter();
+
+        String str = null;
+
+        while ((str = br.readLine()) != null) {
+
+            sw.write(str);
+
+        }
+        return sw.toString();
+    }
+}
